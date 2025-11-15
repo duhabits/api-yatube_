3dd392fff9982709d5cbb27cd3c17d8015b1b9ca
duha <jerebtsow.a@yandex.ru>
2025-11-15 07:00:20 +0300

    12312312312

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,11 +1,10 @@
 from django.shortcuts import get_object_or_404
 from rest_framework import viewsets
-from rest_framework.exceptions import PermissionDenied
 from rest_framework.permissions import IsAuthenticated
 
 from .permissions import IsAuthorOrReadOnly
 from .serializers import CommentSerializer, GroupSerializer, PostSerializer
-from posts.models import Comment, Group, Post
+from posts.models import Group, Post
 
 
 class PostViewSet(viewsets.ModelViewSet):
