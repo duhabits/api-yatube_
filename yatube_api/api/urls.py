744a4334b61c542fcc1981e2d65958112769c629
duha <jerebtsow.a@yandex.ru>
2025-11-26 15:51:19 +0300

    done

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -1,19 +1,15 @@
 from django.urls import include, path
 from rest_framework.authtoken import views
 from rest_framework.routers import DefaultRouter
-from rest_framework_nested import routers
 
 from .views import CommentViewSet, GroupViewSet, PostViewSet
 
 router_main = DefaultRouter()
 router_main.register("posts", PostViewSet)
 router_main.register("groups", GroupViewSet)
-
-router_comment = routers.NestedSimpleRouter(
-    router_main, "posts", lookup="post"
+router_main.register(
+    r'posts/(?P<post_id>\d+)/comments', CommentViewSet, basename='comments'
 )
-router_comment.register("comments", CommentViewSet, basename="comments")
-
 urlpatterns = [
     path("v1/", include(router_main.urls)),
     path("v1/api-token-auth/", views.obtain_auth_token),
--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -22,15 +22,13 @@ class CommentViewSet(viewsets.ModelViewSet):
 
     def get_queryset(self):
         return get_object_or_404(
-            Post, pk=self.kwargs.get('post_pk')
+            Post, pk=self.kwargs.get('post_id')
         ).comments.all()
 
     def perform_create(self, serializer):
         serializer.save(
             author=self.request.user,
-            post=get_object_or_404(
-                Post, pk=get_object_or_404(Post, pk=self.kwargs.get('post_pk'))
-            ),
+            post=get_object_or_404(Post, pk=self.kwargs.get('post_id')),
         )
 
 
